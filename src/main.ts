0b2229bf3d3d045decb5d33452180f88a3bbf9af
xll <mrazxl1991@163.com>
2024-05-10 17:04:58 +0800

    update pro page

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 const app = createApp(App)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(ElementPlus, {
+  locale: zhCn
+})
 app.use(createPinia())
 app.use(router)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,85 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+const Login = () => import('@/views/Login/Login.vue')
+const Sign = () => import('@/views/Sign/Sign.vue')
+const Home = () => import('@/views/Home/Home.vue')
+const Exception = () => import('@/views/Exception/Exception.vue')
+const Check = () => import('@/views/Check/Check.vue')
+const Apply = () => import('@/views/Apply/Apply.vue')
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    menu?: boolean
+    title?: string
+    icon?: string
+    auth?: boolean
+  }
+}
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: Login
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/home',
+      name: 'home',
+      component: Home,
+      redirect: '/sign',
+      meta: {
+        menu: true,
+        title: '考勤管理',
+        icon: 'document-copy',
+        auth: true
+      },
+      children: [
+        {
+          path: '/sign',
+          name: 'sign',
+          component: Sign,
+          meta: {
+            menu: true,
+            title: '在线打卡签到',
+            icon: 'calendar',
+            auth: true
+          }
+        },
+        {
+          path: '/exception',
+          name: 'exception',
+          component: Exception,
+          meta: {
+            menu: true,
+            title: '异常考勤查询',
+            icon: 'warning',
+            auth: true
+          }
+        },
+        {
+          path: '/check',
+          name: 'check',
+          component: Check,
+          meta: {
+            menu: true,
+            title: '我的考勤审批',
+            icon: 'finished',
+            auth: true
+          }
+        },
+        {
+          path: '/apply',
+          name: 'apply',
+          component: Apply,
+          meta: {
+            menu: true,
+            title: '添加考勤审批',
+            icon: 'document-add',
+            auth: true
+          }
+        }
+      ]
     }
   ]
 })
